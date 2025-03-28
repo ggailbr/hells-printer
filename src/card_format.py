6bfbb56a6c61314312e506ee520466fb0ad66285
ggailbr <ggailbr@clemson.edu>
2025-03-27 22:50:21 -0400

    Update downloading to store Metadata. Only grab tokens needed by referenced cards (may not be correct assumption). Remove straggler xml commited

--- src/card_format.py
+++ src/card_format.py
@@ -4,7 +4,17 @@ import dataclasses
 class Card:
     name: str
     image: str
+    def __str__(self):
+        return repr(self.__dict__)
+
 
 @dataclasses.dataclass
 class DraftableCard(Card):
     layout: str
+    def __eq__(self, item):
+        if isinstance(item, str):
+            return item == self.name
+
+@dataclasses.dataclass
+class TokenCard(Card):
+    reverse_related: list[str]
--- src/xml_download_and_label.py
+++ src/xml_download_and_label.py
@@ -1,23 +1,32 @@
 import xml.etree.ElementTree as ET
 import argparse
-import requests
 from pathlib import Path
 import typing
+import tqdm
+import requests
+from PIL import Image
+from io import BytesIO
 
-from card_format import Card, DraftableCard
+from card_format import Card, DraftableCard, TokenCard
 
 
-def download_image(image_url: str, card_name: str, save_folder: Path):
+def download_image(card: Card, save_folder: Path):
     """Downloads an image from a URL and saves it locally with the card name."""
-    response = requests.get(image_url, stream=True)
+    save_folder.mkdir(exist_ok=True)
+    response = requests.get(card.image,  stream=True)
     if response.status_code == 200:
-        image_path = save_folder / f"{card_name}.jpg"
-        with open(image_path, "wb") as file:
-            for chunk in response.iter_content(1024):
-                file.write(chunk)
-        print(f"Downloaded: {image_path}")
+        image_path = save_folder / f"{card.name}.jpg"
+        image_bytes = BytesIO()
+        for chunk in response.iter_content(1024):
+            image_bytes.write(chunk)
+        image_bytes.seek(0)
+        final_image = Image.open(image_bytes)
+        exif = final_image.getexif()
+        exif[0x9286] = str(card)
+        final_image.save(str(save_folder/''.join([a for a in card.name.replace(' ','_').replace("//", "_OR_") if a.isalnum() or a == "_" ]))+".png",exif=exif)
+        print(f"Downloaded: {save_folder/card.name.replace(' ','_')}.png")
     else:
-        print(f"Failed to download {card_name} from {image_url}")
+        print(f"Failed to download {card.name} from {card.image}")
 
 
 def parse_xml_into_cards(xml_path: typing.Union[Path, str], save_folder: Path) -> tuple[list[DraftableCard], list[Card]]:
@@ -35,22 +44,34 @@ def parse_xml_into_cards(xml_path: typing.Union[Path, str], save_folder: Path) -
 
     save_folder.mkdir(parents=True, exist_ok=True)  # Ensure the save directory exists
 
+    # A little inefficient, but doing two passes to only grab tokens that hold references to cards needed
     for card in card_list:
         name = card.find("name").text.replace("/", "_")  # Replace invalid characters
         layout = card.find("prop/layout")
         image_link = card.find("set").attrib.get("picURL")
-
-        if image_link:
-            download_image(image_link, name, save_folder)
-
         if layout is not None:
             cards["draftable_cards"].append(
                 DraftableCard(name, image_link, layout.text)
             )
-        else:
-            cards["tokens"].append(
-                Card(name, image_link)
-            )
+    
+    # Find all tokens that have a reverse related to a card currently parsed
+    # This isn't exact and still requires some manual inspection to remove actual mtg tokens you may not need
+    for card in card_list:
+        name = card.find("name").text.replace("/", "_")  # Replace invalid characters
+        layout = card.find("prop/layout")
+        image_link = card.find("set").attrib.get("picURL")
+        if layout is None:
+            related_cards = card.findall("reverse-related")
+            if len(related_cards) == 0:
+                continue
+            related_list = []
+            for related in related_cards:
+                if related.text in cards["draftable_cards"]:
+                    related_list.append(related.text)
+            if len(related_list) > 0:
+                cards["tokens"].append(
+                        TokenCard(name, image_link, related_list)
+                    )
 
     return cards["draftable_cards"], cards["tokens"]
 
@@ -58,7 +79,13 @@ def parse_xml_into_cards(xml_path: typing.Union[Path, str], save_folder: Path) -
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("xml_file", type=Path, help="Path to the Cockatrice XML file")
-    parser.add_argument("save_folder", type=Path, help="Folder to save card images")
+    parser.add_argument("--save_folder", type=Path, help="Folder to save card images", default=Path(__file__).parent.parent/"downloads")
     args = parser.parse_args()
 
-    parse_xml_into_cards(args.xml_file, args.save_folder)
+    draftable_cards, tokens = parse_xml_into_cards(args.xml_file, args.save_folder)
+    for draftable_card in draftable_cards:
+        download_image(draftable_card, args.save_folder/"draftable")
+    for token in tokens:
+        download_image(token, args.save_folder/"tokens")
+    
+    
