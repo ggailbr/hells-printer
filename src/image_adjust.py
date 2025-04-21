d116dad19be2b7337b78dfdb51c353c242e80a07
fluxWaver <riceblueboy@gmail.com>
2025-04-21 15:08:50 -0400

    Version 1 of image_adjust and upscale modules

--- src/image_adjust.py
+++ src/image_adjust.py
@@ -1,18 +1,30 @@
-#usage: ./image_adjust.py -s <source_path> -d <dest_path>
+# usage: ./image_adjust.py -s <source_path> -d <dest_path>
 # -<source_path> must have "draftable" and "tokens" folders with card images
-# code will place outputs in <dest_path>/draftable and <dest_path>/tokens
 
-# VERSION 1:
-# - Code trusts the 'layout' field completely
-#   - split layout is split down the middle, then saved as two portrait images
-#   - normal layout is simply rotated if not portrait
+# BASICS:
+# - This program takes downloaded images and splits and/or rotates them to be portrait MTG cards
+# - Splitting logic - SPLIT IF:
+#         - card is <split> layout and image is landscape
+#              - if the card (when rotated to portrait) would be the right size, do not split
+#                   - attempts to account for <layout> incorrectness (Fuse, adventure, etc.)
+#
+# - After splitting, cards are rotated to portrait if needed and saved in dest_path/draftable. By 
+#   default, dest_path is <hells-printer\adjusted>. Default source_path is <hells-printer\downloads>
+#
+# - Script is intended to be run after the xml_download_and_label.py script has placed cards in
+#   the <hells-printer\downloads> folder. Should be run from the \src directory 
 
-# - Code does minimal dimension changes
-#   - if not in the right ratio for a card, image is stretched or compressed into the shape
+# KNOWN ISSUES:
+# - for HC1, the tokens need basically no adjustment or splitting. Currently, this code
+#   does nothing to tokens, and simply copies the tokens folder from the source to destination
+#
+# - The code currently completely deletes the destination folder if it already exists, which is 
+#   inefficient but simpler. THE SOURCE AND DESTINATION SHOULD ONLY CONTAIN A <draftable> AND 
+#   <tokens> DIRECTORY WITH THE APPROPRIATE IMAGES. ANY OTHER FILES RISK BEING DELETED
+#
+# - Splitting logic is known to be flawed. It is important to check that all cards that were split
+#   should've been split, and vice versa
 
-# - Code does DPI resizing
-#   - If images resolution is < 744x1039 (aka < 300 DPI):
-#       - images is upscaled with <> to 300 DPI and DPI metadata changed to 300
 
 import sys
 import ast
@@ -20,143 +32,150 @@ import os
 import argparse
 from pathlib import Path
 from PIL import Image
-from io import BytesIO
+import shutil
+import touch_up
 
 #default directories
-source_path = Path('../downloads')
-adj_path = Path('../adjusted')
-dest_path = Path('../final')
+source_path = Path('../downloads').resolve()
+dest_path = Path('../adjusted').resolve()
 
-card_types = ['draftable', 'tokens']
-
-
-standard_dpi = 300 # required pixels/inch
-# portrait card size
-RATIO = 63/88
-DPI = 300
-
-ratio_mm = [63,88]
-ratio_in = [2.48,3.46]
-ratio_px = [744,1039] # for 300 dpi 
-
-#for token in os.listdir(r'..\downloads\tokens'):
-#    print(token)
+# constants
+RATIO = 744/1039 # portrait card size (~0.71591)
+RATIO_ERROR = 0.005 # margin of error for ratio checking
 
 def check_ratio(image: Image) -> bool:
     w, h = image.size
-    if 0.710 <= w/h <= 0.720:
+    if RATIO-RATIO_ERROR <= w/h <= RATIO+RATIO_ERROR:
          return True
     else:
          return False
 
-def adjust(src_path: Path, save_path: Path):
+def adjust(src: Path, dst: Path, type: str):
+     src = src/type
+     dst = dst/type
+
+     # tokens seem to need little processing... for now just copy source directory
+     if type == 'tokens':
+          if dst.exists():
+               shutil.rmtree(dst)
+          try:
+               dst = (dst/'..').resolve()
+               shutil.copytree(src, dst/type)
+               print('Tokens copied from ' +str(src) + ' to ' +str(dst))
+          except:
+               print('Error copying tokens from '+str(src) + ' to ' +str(dst))
+          return
+          
+     # if we are here... processing draftable cards
      warning_list = []
      split_list = []
+     maybe_list = []
+     if dst.exists():
+          shutil.rmtree(dst) # extremely inefficient, removes entire destination if it already exists
+     dst.mkdir(parents=True, exist_ok=True)
 
-     for card in src_path.iterdir():
-        split = False
-        new_cards = []
+     for card in src.iterdir():
+          split_halves = {}
+          save_path = card
+          portrait = True
 
-        # open card image and extract EXIF
-        cur_card = Image.open(str(card))
-        cur_w, cur_h = cur_card.size
-        metadata = (dict(cur_card.getexif()))[37510]
-        metadata = ast.literal_eval(metadata)
-    
-        # find card orientation
-        if(cur_w > cur_h):
-             portrait = False
-        else:
-             portrait = True
+          # open card image and extract EXIF
+          try:
+               current = Image.open(card)
+          except:
+               print('error with '+str(card))
+               return
+          w, h = current.size
+          cur_meta = ast.literal_eval((dict(current.getexif()))[37510])
+          #print(cur_meta)
 
-        # if horizontal and <split>, assume the two cards are actually portrait
-        # if portrait and <split>, dont split
-        if(metadata['layout'] == 'split' and (not portrait)):
-             split_list.append(cur_card)
-             split = True
-             midpoint = cur_w // 2
-             left_half = cur_card.crop((0, 0, midpoint, cur_h))
-             right_half = cur_card.crop((midpoint, 0, cur_w, cur_h))
-             #portrait = True
+          # card orientation
+          if(w > h): portrait = False
 
+          # if card is horizontal and <split> the split into two portrait halves
+          # otherwise, don't split
+          if(cur_meta['layout'] == 'split' and (not portrait)):
+               if not check_ratio(current.rotate(90, expand=1)):
+                    split_list.append(cur_meta['name'])
 
-             left_meta = metadata
-             left_meta['name'] = metadata['name'] + '_LEFT'
-             left_exif = left_half.getexif()
-             left_exif[0x9286] = str(left_meta)
-             new_cards.append(left_half)
-             left_path = save_path / left_meta['name']
+                    midpoint = w // 2
+                    left = current.crop((0, 0, midpoint, h))
+                    right = current.crop((midpoint, 0, w, h))
 
-             right_meta = metadata
-             right_meta['name'] = metadata['name'] + '_RIGHT'
-             right_exif = right_half.getexif()
-             right_exif[0x9286] = str(right_meta)
-             new_cards.append(right_half)
-             right_path = save_path / right_meta['name']
-        else:
-             if not portrait:
-                  cur_card = cur_card.rotate(90, expand=1)
-             new_cards.append(cur_card)
+                    split_halves['left'] = left
+                    split_halves['right'] = right
+                    print('Splitting card: '+card.name)
+               else:
+                    maybe_list.append(cur_meta['name'])
+                    current = current.rotate(90, expand=1)
+                    print('Skipping over a <split> card: '+card.name)
+                    portrait = True
+          else:
+               if not portrait:
+                    current = current.rotate(90, expand=1)
+                    print('Rotated card: '+card.name)
+                    portrait = True
 
-          # new_cards now has either cur_card OR 2 halves of a card
+          save_path = dst/card.name
+          if not split_halves: # split didn't occur
+               if not check_ratio(current):
+                    print('Ratio incorrect on: '+cur_meta['name'])
+                    warning_list.append(cur_meta['name'])
+               current.save(save_path)
+          else: #there are two halves to deal with
+               l_save_path = save_path.with_suffix('').resolve()
+               l_save_path = save_path.with_name(l_save_path.name + '_1.png')
 
-        for index, card in enumerate(new_cards):
-            if not check_ratio(card):
-                 warning_list.append(card)
-          
-            if split:
-                 if index == 0:
-                      left_path = dest_path / left_meta['name']
-                      card.save(left_path, exif=left_exif, format='png')
-                 elif index == 1:
-                      right_path = dest_path / right_meta['name']
-                      card.save(right_path, exif=right_exif, format='png')
-            else:
-                 card.save(save_path / metadata['name'], format='png')
+               r_save_path = save_path.with_suffix('').resolve()
+               r_save_path = save_path.with_name(r_save_path.name + '_2.png')
 
-     print("These cards were split into two:")
-     print(split_list)
-     print("These cards didn't have the correct ratio and should be reviewed:")
+               split_halves['left'].save(l_save_path, format='png')
+               split_halves['right'].save(r_save_path, format='png')
+
+     print('THESE CARDS HAD SUS RATIOS AND SHOULD BE REVIEWED:')
      print(warning_list)
-                 
+     print('THESE CARDS WERE SPLIT AND SHOULD BE REVIEWED:')
+     print(split_list)
+     print('THESE CARDS MAYBE SHOULD HAVE BEEN SPLIT BUT WERE NOT: ')
+     print(maybe_list)
 
-         
-        #saved adjusted cards
+               
 
-            
 
-        # trim corners with gage
 
-        # upscale
+
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--source', type=Path, help='Path to images to be adjusted (draftable and tokens)')
-    parser.add_argument('-d', '--dest', type=Path, help='Directory to save split+upscaled images')
-    args = parser.parse_args()
+     global source_path
+     global dest_path
 
-    global source_path
-    global dest_path
+     parser = argparse.ArgumentParser()
+     parser.add_argument('-s', '--source', type=Path, help='Path to images to be adjusted (draftable and tokens)')
+     parser.add_argument('-d', '--dest', type=Path, help='Directory to save split/adjusted images')
+     args = parser.parse_args()
 
-    if args.source is not None:
-        source_path = args.source
-    if args.dest is not None:
-         dest_path = args.dest
-      
+     if args.source is not None:
+          source_path = args.source
+     if args.dest is not None:
+          dest_path = args.dest
 
+     print('Attempting to adjust from '+str(source_path)+' to '+str(dest_path))
+     if (not (source_path/"draftable").exists()) or (not (source_path/"tokens").exists()):
+          print('Source directory needs to have \"draftable\" and \"tokens\" folders')
+          return
+     
 
-    for element in card_types:
-     start_path = source_path / element
-     end_path = dest_path / element
-     end_path.mkdir(parents=True, exist_ok=True)
-     adjust(src_path=start_path, save_path=end_path)
-    
+     adjust(source_path, dest_path, 'draftable')
+     adjust(source_path, dest_path, 'tokens')
 
+     # then run (but need to make trimmed directory first)
+     # python .\touch_up.py ..\adjusted\draftable\ --output-dir ..\trimmed\draftable\ --max-processes 16
+     # python .\touch_up.py ..\adjusted\tokens\ --output-dir ..\trimmed\tokens\ --max-processes 16
 
-             
+     # then run
+     # python .\upscale.py
+
 
 
 if __name__ == '__main__':
      main()
-
-# end result = image that only needs bleed. (dpi scaled)
